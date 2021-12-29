a5310aacf5216a8a6a062837a0ad44d1ca7ad6ed
si0005hp <si0005hp@gmail.com>
2021-12-29 16:50:36 +0900

    (fix) GC add log

--- src/memory.cpp
+++ src/memory.cpp
@@ -6,19 +6,43 @@ namespace lox {
 
   void Memory::collectGarbage() {
 #ifdef DEBUG_LOG_GC
-    printf("-- gc begin\n");
+    printf("==> gc begin\n");
 #endif
 
+#ifdef DEBUG_LOG_GC
+    printf("--> mark roots begin\n");
+#endif
     vm_->gcMarkRoots();
-
-    vm_->gcBlackenObjects();
-
-    vm_->gcRemoveWeakReferences();
-
-    vm_->gcSweep();
+#ifdef DEBUG_LOG_GC
+    printf("<-- mark roots end\n");
+#endif
 
 #ifdef DEBUG_LOG_GC
-    printf("-- gc end\n");
+    printf("--> blacken objects begin\n");
+#endif
+    vm_->gcBlackenObjects();
+#ifdef DEBUG_LOG_GC
+    printf("<-- blacken objects end\n");
+#endif
+
+#ifdef DEBUG_LOG_GC
+    printf("--> remove weak references begin\n");
+#endif
+    vm_->gcRemoveWeakReferences();
+#ifdef DEBUG_LOG_GC
+    printf("<-- remove weak references end\n");
+#endif
+
+#ifdef DEBUG_LOG_GC
+    printf("--> sweep begin\n");
+#endif
+    vm_->gcSweep();
+#ifdef DEBUG_LOG_GC
+    printf("<-- sweep end\n");
+#endif
+
+#ifdef DEBUG_LOG_GC
+    printf("<== gc end\n");
 #endif
   }
 
